8f1839be18472b69308213d1f9d759c81e8a2456
Flávio Antônio <flavusantonio@gmail.com>
2025-10-07 15:43:27 -0300

    Adicionei o favicom

--- hotdog/src/components/Header.jsx
+++ hotdog/src/components/Header.jsx
@@ -12,7 +12,7 @@ className= "bg-cover bg-no-repeat bg-center w-full py-8 px-8 p-8  pb-8 " style={
     <header className=" text-black  gap-4 flex flex-col justify-center items-center py-8 px-8 p-8  pb-8 shadow-md">
         <h1 className="text-3xl    text-red-700 px-2 font-extrabold bg-amber-300 rounded-full">Hot Dog Gourmet</h1>
        
-        <div className="bg-green-500 text-white rounded-full font-bold"><p className="px-2"> Seg. à Dom. 20h às 01h </p></div>
+        <div className="bg-green-500 text-white rounded-4xl font-bold"><p className="px-2"> Seg. à Dom. 20h às 01h </p></div>
 
          <a
         href="https://wa.me/5521977496651"
@@ -20,8 +20,8 @@ className= "bg-cover bg-no-repeat bg-center w-full py-8 px-8 p-8  pb-8 " style={
         rel="noopener noreferrer"
         className="
           absolute right-4 top-1 py-2  
-          bg-green-600
-          text-green-500 p-3 rounded-full shadow
+          bg-green-400
+          text-green-500 p-3 rounded-xl shadow
           hover:scale-105 transition-transform
         "
         aria-label="Whatsapp"
